6e574ea944047844dfa2f428482f9280e57a8152
Zoltan Kocsis <zoltan@kyanite.keg.data61.csiro.au>
2019-12-05 17:10:58 +1100

    Implement new syntax

--- src/main/scala/Indentable.scala
+++ src/main/scala/Indentable.scala
@@ -5,26 +5,34 @@ object TestObject {
 }
 
 sealed trait Indentable {
-  def <>(that: Indentable) = Indentable.Concat(this,that)
+  def ++(that: Indentable) = Indentable.Concat(this,that)
 
-  def <|>(that: Indentable) = this <> Indentable.SoftBreak <> that
-  
-  def aligned: Indentable = Indentable.WithCurrentColumn { c =>
-    Indentable.WithCurrentLevel { l =>
-      Indentable.AddLevel(c - l, this)
-    }
+  def ++||(that: Indentable) = this ++ Indentable.Literal(" ") ++ Indentable.SoftBreak ++ that
+
+  def ++|(that: Indentable) = this ++ Indentable.Literal(" ") ++ that
+
+  def >>(that: Indentable) = this ++ Indentable.WithCurrentColumn { c =>
+    Indentable.SetLevel(c, that)
   }
 
-  def <<>(that: Indentable) = (this <> that).aligned
+  def >>||(that: Indentable) = this ++|| Indentable.WithCurrentColumn { c =>
+    Indentable.SetLevel(c, that)
+  }
+  
+  def >>|(that: Indentable) = this ++| Indentable.WithCurrentColumn { c =>
+    Indentable.SetLevel(c, that)
+  }
 
-  def <<|>(that: Indentable) = (this <|> that).aligned
-
-  def <>>(that: Indentable) = Indentable.StrainToRightBegin(this, that)
+  def <<(that: Indentable) = Indentable.StrainToRightBegin(this, that)
+  /* The external API should probably consist of these operations, along with
+   * HardBreak, AddLevel and SetLevel; these operations can be given
+   * well-defined semantics independently of the renderer implementation. */
 }
+
 object Indentable {
   case object Empty extends Indentable
-  case object SoftBreak extends Indentable
-  case object HardBreak extends Indentable
+  case object SoftBreak extends Indentable // if the line is too long, do a line break
+  case object HardBreak extends Indentable // always do a line break
   case class Literal(text: String) extends Indentable
   case class AddLevel(level: Int, body: Indentable) extends Indentable
   case class SetLevel(level: Int, body: Indentable) extends Indentable
@@ -51,68 +59,48 @@ object Indented {
 
 object Render {
 
+  val debug = false
+  def debugPrint(str: String): Unit = {
+    if (debug) println(str)
+  }
+
   case class State(currentLevel: Int, target: Indentable)
   type Pipeline = List[State]
-
-  def canFitIn(width: Int, currentColumn: Int, pipeline: Pipeline): Boolean =
-    if (width < currentColumn) false else pipeline match {
-      case Nil => true
-      case (State(currentLevel,target) :: rest) => target match {
-        case Indentable.Empty =>
-          canFitIn(width, currentColumn, rest)
-        case Indentable.SoftBreak =>
-          canFitIn(width, currentColumn, rest)
-        case Indentable.HardBreak =>
-          true
-        case Indentable.Literal(text) =>
-          canFitIn(width, currentColumn + text.length, rest)
-        case Indentable.AddLevel(level, body) =>
-          canFitIn(width, currentColumn, State(currentLevel + level, body) :: rest)
-        case Indentable.SetLevel(level, body) =>
-          canFitIn(width, currentColumn, State(level, body) :: rest)
-        case Indentable.Concat(left, right) =>
-          canFitIn(
-            width,
-            currentColumn,
-            State(currentLevel, left) :: State(currentLevel, right) :: rest
-          )
-        case Indentable.WithCurrentColumn(body) =>
-          canFitIn(width, currentColumn, State(currentLevel, body(currentColumn)) :: rest)
-        case Indentable.WithCurrentLevel(body) =>
-          canFitIn(width, currentColumn, State(currentLevel, body(currentLevel)) :: rest)
-        case Indentable.StrainToRightBegin(left, right) =>
-          canFitIn(
-            width,
-            currentColumn,
-            State(currentLevel, left) :: State(currentLevel, right) :: rest
-          )
-      }
-    }
 
   def render(width: Int, currentColumn: Int, pipeline: Pipeline): Indented = pipeline match {
     case Nil => Indented.Empty
     case (State(currentLevel,target) :: rest) => target match {
       case Indentable.Empty => Indented.Empty
-      case Indentable.SoftBreak =>
-        if (canFitIn(width,currentColumn,pipeline)) render(
-          width,
-          currentColumn,
-          State(currentLevel, Indentable.Literal(" ")) :: rest
-        ) else Indented.Newline(currentLevel, render(width, currentLevel, rest))
-      case Indentable.HardBreak =>
+      case Indentable.SoftBreak => {
+        debugPrint("In column " + currentColumn + " break over width " + width)
+        if (currentColumn > width)
+           render(width, currentColumn, State(currentLevel,Indentable.HardBreak) :: rest)
+        else render(width, currentColumn, rest)
+      }
+      case Indentable.HardBreak => {
+        debugPrint("Rendering newline")
         Indented.Newline(currentLevel, render(width, currentLevel, rest))
-      case Indentable.Literal(text) =>
+      }
+      case Indentable.Literal(text) => {
+        debugPrint("In column " + currentColumn + " rendering literal '" + text ++ "'")
         Indented.Literal(text, render(width, currentColumn + text.length, rest))
-      case Indentable.AddLevel(level, body) =>
+      }
+      case Indentable.AddLevel(level, body) => {
         render(width, currentColumn, State(currentLevel + level, body) :: rest)
-      case Indentable.SetLevel(level, body) =>
+      }
+      case Indentable.SetLevel(level, body) => {
         render(width, currentColumn, State(level, body) :: rest)
-      case Indentable.Concat(left, right) =>
-        render(width, currentColumn, State(currentLevel, left) :: State(currentLevel, right) :: rest )
-      case Indentable.WithCurrentColumn(body) =>
+      }
+      case Indentable.Concat(left, right) => {
+        render(width, currentColumn, State(currentLevel,left) :: State(currentLevel,right) :: rest )
+      }
+      case Indentable.WithCurrentColumn(body) => {
+        debugPrint("Call with current column " + currentColumn)
         render(width, currentColumn, State(currentLevel, body(currentColumn)) :: rest)
-      case Indentable.WithCurrentLevel(body) =>
+      }
+      case Indentable.WithCurrentLevel(body) => {
         render(width, currentColumn, State(currentLevel, body(currentLevel)) :: rest)
+      }
       case Indentable.StrainToRightBegin(left, right) => {
         render(width, currentColumn, State(currentLevel, right) :: rest) match {
           case Indented.Newline(level,_) => render(
@@ -129,6 +117,8 @@ object Render {
       } // end target match
     } // end pipeline match
   } // end def render
+
+  def apply(target: Indentable, width: Int): Indented = render(width,0,List(State(0,target)))
 
   def apply(target: Indentable): Indented = render(80,0,List(State(0,target)))
 
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -17,37 +17,74 @@ object Main {
    * ```
    */
 
-  val command1: Indentable =
-    HardBreak <> Literal("apply") <|> Literal("(") <> Literal("simp") <|> (
-      Literal("add:") <<|> (
-        Literal("conjI") <|> Literal("disjE") <|> Literal("impI")
+  val command1: Indentable = // Isabelle commands start with a hard break
+    HardBreak ++ (
+      Literal("apply") >>| (
+        Literal("(") ++ (
+          Literal("simp") >>|| (
+            Literal("add:") >>|| (
+              Literal("conjI") ++|| (
+                Literal("disjI") ++|| (
+                  Literal("impI") ++ (
+                    Literal(")")
+                  )
+                )
+              )
+            )
+          )
+        )
       )
-    ) <> Literal(")")
+    )
 
   val command2: Indentable =
-    HardBreak <> Literal("apply") <|> Literal("(") <> Literal("induction") <|>
-    Literal("n") <> Literal(")")
+    HardBreak ++ (
+      Literal("apply") >>| (
+        Literal("(") ++ (
+          Literal("induction") >>|| (
+            Literal("n") ++ (
+              Literal(")")
+            )
+          )
+        )
+      )
+    )
 
   val comment1: Indentable =
-    HardBreak <> Literal("(* comment aligns with new line *)")
+    HardBreak ++ (
+      Literal("(* comment aligns with next line *)")
+    )
 
   val command3: Indentable =
-    HardBreak <> Literal("apply") <|> Literal("simp")
+    HardBreak ++ (
+      Literal("apply") >>| (
+        Literal("simp")
+      )
+    )
 
   val lookup1: Indentable =
-    SetLevel(0, HardBreak <> Literal("find_theorems") <|> Literal("blah"))
+    SetLevel(0,
+      HardBreak ++ (
+        Literal("find_theorems") >>| (
+          Literal("blah")
+        )
+      )
+    )
 
   val command4: Indentable =
-    HardBreak <> Literal("apply") <|> Literal("arith")
+    HardBreak ++ (
+      Literal("apply") >>| (
+        Literal("arith")
+      )
+    )
 
   val example: Indentable =
-    command1 <> AddLevel(0,
-      command2 <> AddLevel(1,
-        comment1 <>> (
-          command3 <> AddLevel(0, 
-            comment1 <>> (
-              lookup1 <> (
-                command4 <> AddLevel(-1, 
+    command1 ++ AddLevel(0,
+      command2 ++ AddLevel(1,
+        comment1 << (
+          command3 ++ AddLevel(0, 
+            comment1 << (
+              lookup1 ++ (
+                command4 ++ AddLevel(-1, 
                   command3
                 )
               )
@@ -57,14 +94,18 @@ object Main {
       )
     )
 
+  def printExample(columns: Int): Unit = {
+    println("\n" + columns + " columns")
+    println( List.fill(columns)('-').mkString )
+    println( Render(example,columns-3).toText )
+  }
+
   def main(args: Array[String]): Unit = {
     println("If you're reading this, the build worked!")
-    println("\nWe will render the following Indentable: ")
-    println( example.toString )
-    println("\nIt yields the following Indented: ")
-    println( Render(example) )
-    println("\nRendered, it looks as follows: ")
-    println( Render(example).toText )
+    printExample(10)
+    printExample(20)
+    printExample(30)
+    printExample(40)
   }
   
 }
